0801ecec10a862edf5ff6c18bc1fb809599b41ad
Joaquin Pacis <pacisljoaquin0@gmail.com>
2026-06-03 17:02:47 +0800

    Revert "Failed 3 test regarding removing non same-type values, add continue logic to skip items"
    This reverts commit 66e37e9d911e2f118001e354595f0d4abfc6b401.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    let sum = 0;
+    
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
 
+    const start = Math.min(a,b);
+    const end = Math.max(a,b);
+
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    } 
+
+    return sum;
 };
+
+/* 
+1. Check numbers in between by iteration
+3. Check if negative
+2. Sum numbers in start, between, and end
+
+
+*/
 
 // Do not edit below this line
 module.exports = sumAll;
