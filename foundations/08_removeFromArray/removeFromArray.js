66e37e9d911e2f118001e354595f0d4abfc6b401
Joaquin Pacis <pacisljoaquin0@gmail.com>
2026-06-03 14:59:41 +0800

    Failed 3 test regarding removing non same-type values, add continue logic to skip items

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...a) {
+    let arrayCount = {};
+    let merged = arr.concat(a);
+    
+    for(const item of merged) {
+        arrayCount[item] = (arrayCount[item] || 0) + 1;
+    }
+    
+    let arrayMerged = merged.filter(item => arrayCount[item] === 1);
+    
+    return arrayMerged;
 };
+
+
+/* 
+
+1. Merge array and items to be removed
+2. Match similar items 
+3. Remove similar items
+4. Return to array
+
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
